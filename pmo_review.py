23f4dfe99eff205a24b116d94772a5055c5c9785
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 22:23:34 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -11,27 +11,7 @@ uploaded_files = st.sidebar.file_uploader("Choose a file",type=['CSV'], accept_m
 if uploaded_files is not None:
     for file in uploaded_files:
         file.seek(0)
-    df = pd.read_csv(file, header=[0], encoding='latin1')
-    concat_df = pd.concat(df)
+        df = pd.read_csv(file, header=[0], encoding='latin1')
+        concat_df = pd.concat(df)
         
-#    df['Start'] = df['Start'].astype('datetime64')
-#        df['Finish'] = df['Finish'].astype('datetime64')
-#        df['CR'] = df['CR'].astype(str)
-    
-#        orders = list(df['CR'])
-    
-    st.write(concat_df)
-    
-
-#    fig = px.timeline(df
-#                      , x_start="Start"
-#                      , x_end="Finish"
-#                      , y="CR"
-#                      , hover_name="CR"
-#                      , color='Status'
-#                      , opacity=.7
-#    )
-    
-#    fig.update_yaxes(autorange="reversed")     
-    
-#    fig
+        st.write(concat_df)
