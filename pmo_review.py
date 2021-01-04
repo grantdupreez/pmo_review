a39239d89ddb9a205db94108feb8323516ffc8e9
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 21:35:58 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -6,10 +6,10 @@ st.write("**Upload project plans for review**")
 st.write("Use csv files")
 
 uploaded_files = st.sidebar.file_uploader("Choose CSV files", type='csv')
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+#for uploaded_file in uploaded_files:
+#    bytes_data = uploaded_file.read()
+#    st.write("filename:", uploaded_file.name)
+#    st.write(bytes_data)
 
 
 
@@ -17,11 +17,11 @@ for uploaded_file in uploaded_files:
 
 
 
-#if uploaded_files:
+if uploaded_files:
 #    for file in uploaded_files:
 #        file.seek(0)
-#    uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
-#    raw_data = pd.concat(uploaded_data_read)
-#    
-#    st.write(raw_data)
-#
+    uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
+    raw_data = pd.concat(uploaded_data_read)
+    
+    st.write(raw_data)
+
