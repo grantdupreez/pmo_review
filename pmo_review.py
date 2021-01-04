5a9499b189b18cf454f7ff6759e42dc281003859
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 21:43:47 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -1,10 +1,24 @@
-import streamlit as st
 import os
+import streamlit as st
 
-def file_selector(folder_path='.'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
 
-filename = file_selector()
-st.write('You selected `%s`' % filename)
+def st_file_selector(st_placeholder, path='.', label='Please, select a file/folder...'):
+    # get base path (directory)
+    base_path = '.' if path is None or path is '' else path
+    base_path = base_path if os.path.isdir(
+        base_path) else os.path.dirname(base_path)
+    base_path = '.' if base_path is None or base_path is '' else base_path
+    # list files in base path directory
+    files = os.listdir(base_path)
+    if base_path is not '.':
+        files.insert(0, '..')
+    files.insert(0, '.')
+    selected_file = st_placeholder.selectbox(
+        label=label, options=files, key=base_path)
+    selected_path = os.path.normpath(os.path.join(base_path, selected_file))
+    if selected_file is '.':
+        return selected_path
+    if os.path.isdir(selected_path):
+        selected_path = st_file_selector(st_placeholder=st_placeholder,
+                                         path=selected_path, label=label)
+    return selected_path
