c29e82631ba90b318cdc0e1e4deab1fc65c7d4fd
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 20:49:07 +0000

    Create pmo_review.py

---
+++ pmo_review.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objs as go
+
+st.write("**Upload project plans for review**")
+st.write("Use csv files")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file",type=['CSV'])
+
+uploaded_files = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=True)
+if uploaded_files:
+    for file in uploaded_files:
+        file.seek(0)
+    uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
+    raw_data = pd.concat(uploaded_data_read)
+    
+    st.write(raw)
+    
