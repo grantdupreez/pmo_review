ad7a7c09209746a43ca6f5191942a71222ef7714
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 21:46:11 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -1,24 +1,16 @@
-import os
 import streamlit as st
+import pandas as pd
+import io
 
+st.write("**Upload project plans for review**")
+st.write("Use csv files")
+	
 
-def st_file_selector(st_placeholder, path='.', label='Please, select a file/folder...'):
-    # get base path (directory)
-    base_path = '.' if path is None or path is '' else path
-    base_path = base_path if os.path.isdir(
-        base_path) else os.path.dirname(base_path)
-    base_path = '.' if base_path is None or base_path is '' else base_path
-    # list files in base path directory
-    files = os.listdir(base_path)
-    if base_path is not '.':
-        files.insert(0, '..')
-    files.insert(0, '.')
-    selected_file = st_placeholder.selectbox(
-        label=label, options=files, key=base_path)
-    selected_path = os.path.normpath(os.path.join(base_path, selected_file))
-    if selected_file is '.':
-        return selected_path
-    if os.path.isdir(selected_path):
-        selected_path = st_file_selector(st_placeholder=st_placeholder,
-                                         path=selected_path, label=label)
-    return selected_path
+uploaded_files = st.sidebar.file_uploader("Choose CSV files", type='csv')
+	if uploaded_files:
+	#    for file in uploaded_files:
+	#        file.seek(0)
+	    uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
+	    raw_data = pd.concat(uploaded_data_read)
+	    
+	    st.write(raw_data)
