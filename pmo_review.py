01f4a2047274d968e9f633f21c545619054112b1
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 22:18:44 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -10,17 +10,17 @@ st.write("Use the template csv file")
 uploaded_files = st.sidebar.file_uploader("Choose a file",type=['CSV'], accept_multiple_files=True)
 if uploaded_files is not None:
     for file in uploaded_files:
-        df = pd.read_csv(file, header=[0], encoding='latin1')
         file.seek(0)
-        st.write(df)
+    df = pd.read_csv(file, header=[0], encoding='latin1')
+    concat_df = pd.concat(df)
+        
+#    df['Start'] = df['Start'].astype('datetime64')
+#        df['Finish'] = df['Finish'].astype('datetime64')
+#        df['CR'] = df['CR'].astype(str)
     
-        df['Start'] = df['Start'].astype('datetime64')
-        df['Finish'] = df['Finish'].astype('datetime64')
-        df['CR'] = df['CR'].astype(str)
+#        orders = list(df['CR'])
     
-        orders = list(df['CR'])
-    
-        st.write(df)
+    st.write(concat_df)
     
 
 #    fig = px.timeline(df
