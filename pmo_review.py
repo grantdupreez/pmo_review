9873d604222657dd767e4711691418f1e89c1507
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 21:47:11 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -7,10 +7,9 @@ st.write("Use csv files")
 	
 
 uploaded_files = st.sidebar.file_uploader("Choose CSV files", type='csv')
-	if uploaded_files:
-	#    for file in uploaded_files:
-	#        file.seek(0)
-	    uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
-	    raw_data = pd.concat(uploaded_data_read)
-	    
-	    st.write(raw_data)
+if uploaded_files:
+#    for file in uploaded_files:
+#        file.seek(0)
+	uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
+	raw_data = pd.concat(uploaded_data_read)
+	st.write(raw_data)
