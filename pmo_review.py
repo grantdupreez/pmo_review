976431444e1faf08ca5e9d629f0fa1f7cc6fc67a
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 20:53:42 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -7,14 +7,12 @@ import plotly.graph_objs as go
 st.write("**Upload project plans for review**")
 st.write("Use csv files")
 
-uploaded_file = st.sidebar.file_uploader("Choose a file",type=['CSV'])
-
-uploaded_files = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=True)
+uploaded_file = st.sidebar.file_uploader("Choose a file",type=['CSV'], accept_multiple_files=True)
 if uploaded_files:
     for file in uploaded_files:
         file.seek(0)
     uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
     raw_data = pd.concat(uploaded_data_read)
     
-    st.write(raw)
+    st.write(raw_data)
     
