dda3e7d758a68777a83fcbb0689ffb616d1945a9
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-04 21:33:56 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -5,7 +5,7 @@ import io
 st.write("**Upload project plans for review**")
 st.write("Use csv files")
 
-uploaded_files = st.sidebar.file_uploader("Choose CSV files", type='csv', accept_multiple_files=True)
+uploaded_files = st.sidebar.file_uploader("Choose CSV files", type='csv')
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
