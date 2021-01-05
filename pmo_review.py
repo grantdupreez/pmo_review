e30b67bef2e71f46dea0365bfa591e48c299dce8
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-05 13:05:26 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -11,9 +11,9 @@ list_of_dataframes = []
 uploaded_files = st.sidebar.file_uploader("Choose a file",type=['CSV'], accept_multiple_files=True)
 if uploaded_files is not None:
     for file in uploaded_files:
-#        file.seek(0)
+        file.seek(0)
         list_of_dataframes.append(pd.read_csv(file, header=[0], encoding='latin1')
     
-    merged_df = pd.concat(list_of_dataframes)                                  
-        
+    merged_df = pd.concat(list_of_dataframes)
+    
     st.write(merged_df)
