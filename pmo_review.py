cc8b9ac833f1fc029e6425e039a122e2a333d186
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-06 15:50:41 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -64,7 +64,7 @@ if uploaded_file is not None:
     colorscale = [[0, 'lightsteelblue'], [1, 'mediumseagreen']];
 
     fig = go.Figure(data = [go.Parcats(dimensions=[country_dim, pm_dim, proc_dim, prio_dim, state_dim, rag_dim],
-        line={'color': color, 'colorscale': colorscale},
-        hoveron='color')])
+        hoveron={'color': color, 'colorscale': colorscale}
+                                      )])
 
     fig
