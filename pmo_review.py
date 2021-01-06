8cec8bb6a3d3dbf7621eb194549750eb9c182ca2
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-06 15:24:02 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -62,6 +62,6 @@ if uploaded_file is not None:
     # Create parcats trace
     color = df.Process;
 
-    fig = go.Figure(data = [go.Parcats(dimensions=[proc_dim, prio_dim, state_dim, country_dim, pm_dim, rag_dim])])
+    fig = go.Figure(data = [go.Parcats(dimensions=[country_dim, pm_dim, proc_dim, prio_dim, state_dim, rag_dim])])
 
     fig
