c86394e9132ece02bf7fe7849bf69617deb76559
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-06 12:37:37 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -23,7 +23,8 @@ st.sidebar.title("Upload the template")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file",type=['CSV'])
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, header=[0], encoding='latin1')
+#    df = pd.read_csv(uploaded_file, header=[0], encoding='latin1')
+    df = pd.read_csv(uploaded_file, header=[0])
     
 #    df['Start'] = pd.to_datetime(df['Start'])
 
