8d1ee610175b827dc2f29ffd807d8cd508fb45a5
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-11 21:31:18 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -88,3 +88,7 @@ if uploaded_file is not None:
 
 #ACTION
 #    df.to_csv(s3_string+dt_string)
+
+    hist_values = np.histogram(
+    new_df.Priority, len(bins=new_df.Priority))
+    st.bar_chart(hist_values)
