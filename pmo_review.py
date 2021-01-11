1262211fcce7b116ebac9260a6fe7eb42510abce
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-11 17:44:49 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -12,24 +12,31 @@ def show_par_chart(dataframe):
         categoryorder='category ascending', label="Process"
     )
     prio_dim = go.parcats.Dimension(values=new_df.Priority, label="Priority")
+    
     state_dim = go.parcats.Dimension(
         values=new_df.State, label="State"
     )
+    
     country_dim = go.parcats.Dimension(
         values=new_df.Impacted_Countries, label="Countries"
     )
+    
     partner_dim = go.parcats.Dimension(
         values=new_df.Partner, label="Partner"
     )
+    
     funding_dim = go.parcats.Dimension(
         values=new_df.Funding, label="Funding"
     )
+    
     pm_dim = go.parcats.Dimension(
         values=new_df.PM, label="PM"
     )
+    
     rag_dim = go.parcats.Dimension(
         values=new_df.RAG, label="RAG"
-    )   
+    )
+    
     color = new_df.Priority;
     fig = go.Figure(data = [go.Parcats(dimensions=[rag_dim, country_dim, pm_dim, funding_dm, partner_dm, prio_dim, state_dim],
 #    fig = go.Figure(data = [go.Parcats(dimensions=[rag_dim, country_dim, pm_dim, prio_dim, state_dim],
