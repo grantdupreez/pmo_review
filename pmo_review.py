3f83284dcbb945e412849b4d4ecac8629f1fb231
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-11 17:35:28 +0000

    Update pmo_review.py

--- pmo_review.py
+++ pmo_review.py
@@ -18,12 +18,12 @@ def show_par_chart(dataframe):
     country_dim = go.parcats.Dimension(
         values=new_df.Impacted_Countries, label="Countries"
     )
-#    partner_dim = go.parcats.Dimension(
-#        values=new_df.Partner, label="Partner"
-#    )
-#    funding_dim = go.parcats.Dimension(
-#        values=new_df.Funding, label="Funding"
-#    )
+    partner_dim = go.parcats.Dimension(
+        values=new_df.Partner, label="Partner"
+    )
+    funding_dim = go.parcats.Dimension(
+        values=new_df.Funding, label="Funding"
+    )
     pm_dim = go.parcats.Dimension(
         values=new_df.PM, label="PM"
     )
@@ -31,8 +31,8 @@ def show_par_chart(dataframe):
         values=new_df.RAG, label="RAG"
     )   
     color = new_df.Priority;
-#    fig = go.Figure(data = [go.Parcats(dimensions=[rag_dim, country_dim, pm_dim, funding_dm, partner_dm, prio_dim, state_dim, rag_dim],
-    fig = go.Figure(data = [go.Parcats(dimensions=[rag_dim, country_dim, pm_dim, prio_dim, state_dim],
+    fig = go.Figure(data = [go.Parcats(dimensions=[rag_dim, country_dim, pm_dim, funding_dm, partner_dm, prio_dim, state_dim],
+#    fig = go.Figure(data = [go.Parcats(dimensions=[rag_dim, country_dim, pm_dim, prio_dim, state_dim],
         line={'color': ["SkyBlue", "SlateGray", "Silver"]},
         hoveron='color'
         )])
